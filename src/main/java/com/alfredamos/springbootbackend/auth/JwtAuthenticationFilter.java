2823efba57d33e8e0310bbe9a5d03c0129af66fe
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2025-08-29 22:09:03 +0100

    Add cors-config and refactor jwt-authentication-filter.

--- src/main/java/com/alfredamos/springbootbackend/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/alfredamos/springbootbackend/auth/JwtAuthenticationFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,7 +26,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         var cookies = request.getCookies(); //----> Get all cookies.
         var accessToken = mySpecificCookieValue(cookies); //----> Get access token
 
